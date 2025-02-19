9fe053b40c9440ec0b1cc129880863cd35e26ca9
NthnH <110603459+NthnH@users.noreply.github.com>
2025-02-19 18:50:54 +0100

    Update quote.js
    added fallback quote in case of error

--- scripts/quote.js
+++ scripts/quote.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const quoteElement = document.getElementById("quote");
     fetch("https://spaceapi.voidwarranties.be/quotes/random?tags=technology")
-        .then(response => response.json())
-        .then(data => {
-        const quoteElement = document.getElementById("quote");
+    .then(response => response.json())
+    .then(data => {
         quoteElement.innerHTML = `<blockquote>"${data[0].content}"<br /><div class="cite"><cite>${data[0].author}</cite></div></blockquote>`;
-        })
-        .catch(error => console.error('Error fetching quote:', error));
+    })
+    .catch(error => {
+        console.error('Error fetching quote:', error);
+        const content = "(3276019 row(s) affected)";
+        const author = "SQL server";
+        quoteElement.innerHTML = '<blockquote>"'+ content +'"<br /><div class="cite"><cite>'+ author +'</cite></div></blockquote>';
+        console.log("Fallback quote set.");
+    });
 });
